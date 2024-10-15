b21636ff056f4cb20f17bc43e80fd2274363b73e
Alla Prishchepa <tyurina.alla@gmail.com>
2024-10-15 22:11:38 +0400

    feat: implement tests for doStuffByTimeout and doStuffByInterval

--- src/06-mocking-node-api/index.test.ts
+++ src/06-mocking-node-api/index.test.ts
@@ -1,5 +1,4 @@
-// Uncomment the code below and write your tests
-// import { readFileAsynchronously, doStuffByTimeout, doStuffByInterval } from '.';
+import { doStuffByTimeout, doStuffByInterval } from '.';
 
 describe('doStuffByTimeout', () => {
   beforeAll(() => {
@@ -11,11 +10,29 @@ describe('doStuffByTimeout', () => {
   });
 
   test('should set timeout with provided callback and timeout', () => {
-    // Write your test here
+    const setTimeoutSpy = jest.spyOn(global, 'setTimeout');
+
+    const callback = jest.fn();
+    const timeout = 4000;
+
+    doStuffByTimeout(callback, timeout);
+
+    expect(setTimeoutSpy).toHaveBeenCalledWith(callback, timeout);
+
+    setTimeoutSpy.mockRestore();
   });
 
   test('should call callback only after timeout', () => {
-    // Write your test here
+    const callback = jest.fn();
+    const timeout = 4000;
+
+    doStuffByTimeout(callback, timeout);
+
+    expect(callback).not.toHaveBeenCalled();
+
+    jest.advanceTimersByTime(timeout);
+
+    expect(callback).toHaveBeenCalledTimes(1);
   });
 });
 
@@ -29,11 +46,31 @@ describe('doStuffByInterval', () => {
   });
 
   test('should set interval with provided callback and timeout', () => {
-    // Write your test here
+    const setIntervalSpy = jest.spyOn(global, 'setInterval');
+
+    const callback = jest.fn();
+    const timeout = 4000;
+
+    doStuffByInterval(callback, timeout);
+
+    expect(setIntervalSpy).toHaveBeenCalledWith(callback, timeout);
+
+    setIntervalSpy.mockRestore();
   });
 
   test('should call callback multiple times after multiple intervals', () => {
-    // Write your test here
+    const callback = jest.fn();
+    const timeout = 4000;
+
+    doStuffByInterval(callback, timeout);
+
+    expect(callback).not.toHaveBeenCalled();
+
+    jest.advanceTimersByTime(timeout);
+    expect(callback).toHaveBeenCalledTimes(1);
+
+    jest.advanceTimersByTime(timeout * 2);
+    expect(callback).toHaveBeenCalledTimes(3);
   });
 });
 
